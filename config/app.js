223fa7d2ef05bbe6c200723858c1cf118dfcfb56
Artem Vasiliev <artem.job@gmail.com>
2017-12-19 14:46:04 +0300

    chore: removed rc
    dotenv is easier and is enough

--- config/app.js
+++ config/app.js
@@ -1,14 +1,12 @@
-const env = process.env['NODE_ENV'] || 'development';
-const rc = require('rc');
 const dotenv = require('dotenv');
 dotenv.load({silent: true});
 
 const config =  {
   rolloutServiceHost: process.env['SERVICE_HOST'],
   rolloutServicePort: process.env['SERVICE_PORT'] || 443,
-  port: process.env['PORT'] || '{{ key "configurations/microservices/rollout/rollout-dashboard-port" }}',
+  port: process.env['PORT'],
   googleAuth: {
-    clientId: process.env['GAPI_CLIENT_ID'] || '{{ key "configurations/microservices/rollout/google-auth-client-id" }}'
+    clientId: process.env['GAPI_CLIENT_ID']
   }
 };
 
