95292c16286bd6bf9915b06302e4da170ea2b4e8
Artem Vasiliev <artem.job@gmail.com>
2017-12-19 12:29:13 +0300

    chore: removed unneeded key

--- config/app.js
+++ config/app.js
@@ -4,19 +4,12 @@ const dotenv = require('dotenv');
 dotenv.load({silent: true});
 
 const config =  {
-  development: {
-    rolloutServiceHost: process.env['SERVICE_HOST'] || 'http://localhost',
-    rolloutServicePort: process.env['SERVICE_PORT'] || '9999',
-    port: process.env['PORT'] || 1234,
-    googleAuth: rc('gauth', {})
-  },
-  production: {
-    rolloutServiceHost: process.env['SERVICE_HOST'],
-    rolloutServicePort: process.env['SERVICE_PORT'] || 443,
-    port: process.env['PORT'] || '{{ key "configurations/microservices/rollout/rollout-dashboard-port" }}',
-    googleAuth: {
-      clientId: process.env['GAPI_CLIENT_ID'] || '{{ key "configurations/microservices/rollout/google-auth-client-id" }}'
-    }
+  rolloutServiceHost: process.env['SERVICE_HOST'],
+  rolloutServicePort: process.env['SERVICE_PORT'] || 443,
+  port: process.env['PORT'] || '{{ key "configurations/microservices/rollout/rollout-dashboard-port" }}',
+  googleAuth: {
+    clientId: process.env['GAPI_CLIENT_ID'] || '{{ key "configurations/microservices/rollout/google-auth-client-id" }}'
+  }
 };
 
 module.exports = config;
